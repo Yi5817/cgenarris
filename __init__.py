db581690120dcae3180fd9870d7755d90402fce8
haoran-ni <hrni1998@gmail.com>
2026-02-11 22:59:16 -0500

    Fix import error for Genarris Interfaces

--- __init__.py
+++ __init__.py
@@ -2,9 +2,8 @@ try:
     # Genarris
     from gnrs.cgenarris.src import pygenarris_mpi
     from gnrs.cgenarris.src.rpack.rigid_press import rigid_press
+    __all__ = ["pygenarris_mpi", "rigid_press"]
 except:
     # Genarris Interfaces
     from gnrs.generation.cgenarris.src import pygenarris_mpi
-    from gnrs.generation.cgenarris.src.rpack.rigid_press import rigid_press
-
-__all__ = ["pygenarris_mpi", "rigid_press"]
+    __all__ = ["pygenarris_mpi"]
