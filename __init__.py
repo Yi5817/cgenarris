355f0360832fa19554346635868b7b61da2ea58e
haoran-ni <hrni1998@gmail.com>
2026-02-11 22:56:03 -0500

    Fix import error for Genarris Interfaces

--- __init__.py
+++ __init__.py
@@ -1,4 +1,10 @@
-from gnrs.cgenarris.src import pygenarris_mpi
-from gnrs.cgenarris.src.rpack.rigid_press import rigid_press
+try:
+    # Genarris
+    from gnrs.cgenarris.src import pygenarris_mpi
+    from gnrs.cgenarris.src.rpack.rigid_press import rigid_press
+except:
+    # Genarris Interfaces
+    from gnrs.generation.cgenarris.src import pygenarris_mpi
+    from gnrs.generation.cgenarris.src.rpack.rigid_press import rigid_press
 
 __all__ = ["pygenarris_mpi", "rigid_press"]
